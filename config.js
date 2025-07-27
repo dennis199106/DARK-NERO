bbd58682a42d3d951f6fa5050df5ae25f60a3529
dennis199106 <nwankwopauline06@gmail.com>
2025-07-27 17:58:30 +0100

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@ const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '',
+    SESSION_ID: process.env.SESSION_ID || 'GENUX-049413',
     PREFIX: process.env.PREFIX || '.',
-    OWNER_NUMBER: process.env.OWNER_NUMBER || ['94771029271'],
-    MONGODB_URI: process.env.MONGODB_URI || '',
+    OWNER_NUMBER: process.env.OWNER_NUMBER || ['2348139239376'],
+    MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://kakomo7064:F81fvkXvC4xFHmbP@cluster0.rfwkuda.mongodb.net/',
 };
