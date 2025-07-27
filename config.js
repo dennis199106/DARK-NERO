2bd19234a89c90014f94026ca09b5bc32db4bb89
dennis199106 <nwankwopauline06@gmail.com>
2025-07-27 18:06:45 +0100

    Update config.js

--- config.js
+++ config.js
@@ -5,5 +5,5 @@ module.exports = {
     SESSION_ID: process.env.SESSION_ID || 'GENUX-049413',
     PREFIX: process.env.PREFIX || '.',
     OWNER_NUMBER: process.env.OWNER_NUMBER || ['2348139239376'],
-    MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://kakomo7064:F81fvkXvC4xFHmbP@cluster0.rfwkuda.mongodb.net/',
+    MONGODB_URI: process.env.MONGODB_URI || '',
 };
